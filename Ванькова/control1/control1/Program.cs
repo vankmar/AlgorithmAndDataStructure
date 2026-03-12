e0e9726200dc5a4fe626f280a049529ab258a915
vankmar <l05kku70kk@gmail.com>
2026-03-12 15:08:21 +0700

    Add files via upload
    контрольная 1 (12.03.26)

---
+++ Ванькова/control1/control1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace control1
+{
+    internal class Program
+    {
+        static int[] test1 = { 1,2,2,3,4,5};
+        static bool IsAscending(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                return false;
+            }
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] >= array[i + 1] )
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            if (IsAscending(test1) == true)
+            {
+                Console.WriteLine("Массив возрастает");
+            }
+            else
+            {
+                Console.WriteLine("Массив не возрастает");
+            }
+        }
+    }
+}
---
+++ Ванькова/control2/control2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace control2
+{
+    internal class Program
+    {
+        static int n = 4;
+        static int GetFibonacci(int n)
+        {
+            if (n < 0) return -1;
+            int fibonacci = 0;
+            int temp = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (fibonacci == 0)
+                    temp++;
+                if (fibonacci >0)
+                    temp = fibonacci;
+                fibonacci += temp;
+                i++;
+            }
+            return fibonacci;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFibonacci(n));
+        }
+    }
+}
